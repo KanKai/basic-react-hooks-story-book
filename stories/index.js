c3b430d0c5522a2e71cbedb4733c4ce26cb37bb1
jeerawat_k <>
2019-02-11 18:48:17 +0700

    add button documents

--- stories/index.js
+++ stories/index.js
@@ -3,11 +3,35 @@ import { storiesOf } from "@storybook/react";
 import Button from "../src/components/Button";
 import CounterHook from "../src/components/Counter";
 import UserContainer from "../src/containers/UserContainer";
+import {
+  ButtonPrimary,
+  ButtonDanger,
+  ButtonDangerOutline,
+  ButtonPrimaryOutline
+} from "../src/styles";
 
-storiesOf("Button", module).add("click button to alert", () => <Button />);
+const storiesButton = storiesOf("Button", module);
+
+storiesButton
+  .add("Primary", () => <ButtonPrimary>ButtonPrimary</ButtonPrimary>)
+  .add("PrimaryOutline", () => (
+    <ButtonPrimaryOutline>ButtonPrimary Outline</ButtonPrimaryOutline>
+  ))
+  .add("Danger", () => <ButtonDanger>ButtonDanger</ButtonDanger>)
+  .add("DangerOutline", () => (
+    <ButtonDangerOutline>ButtonDanger Outline</ButtonDangerOutline>
+  ))
+  .add("Primary Small", () => <ButtonPrimary sm>ButtonDanger</ButtonPrimary>)
+  .add("Danger Small", () => <ButtonDanger sm>ButtonDanger</ButtonDanger>);
+
+storiesOf("Button Action", module).add("click button to alert", () => (
+  <Button />
+));
+
 storiesOf("CounterHook", module).add("click button to counter number", () => (
   <CounterHook />
 ));
+
 storiesOf("CRUD", module).add("basic CRUD with react hooks", () => (
   <UserContainer />
 ));
